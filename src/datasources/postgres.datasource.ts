d243e4c155f7807b1463a5c4f4635b8f58fb5504
Leonardo <leonardo.scantamburlo@confirmo.it>
2024-01-16 11:09:58 +0100

    Add SSL configuration to Postgres datasource

--- src/datasources/postgres.datasource.ts
+++ src/datasources/postgres.datasource.ts
@@ -10,6 +10,7 @@ const config = {
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: process.env.DB_HOST !== 'localhost'
 };
 
 // Observe application's life cycle to disconnect the datasource when
